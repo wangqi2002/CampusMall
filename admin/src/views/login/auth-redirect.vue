5bbd46fa891f2d4eb2430dc960f717b5fcf3a5c6
wangqi2002 <1722009706@qq.com>
2024-02-29 22:40:52 +0800

    可运行

--- admin/src/views/login/auth-redirect.vue
+++ admin/src/views/login/auth-redirect.vue
@@ -1,5 +1,13 @@
-<template>
-</template>
+<!--
+ * @Author: wangqi2002 1722009706@qq.com
+ * @Date: 2024-02-28 15:06:50
+ * @LastEditors: wangqi2002 1722009706@qq.com
+ * @LastEditTime: 2024-02-28 19:11:02
+ * @FilePath: \CampusMall\admin\src\views\login\auth-redirect.vue
+ * @Description:
+ *
+-->
+<template />
 <script>
 export default {
   name: 'AuthRedirect',
